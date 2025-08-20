72931de9ad32a2de2c47d42d3cc32f617f88a332
Мар'ян Верещак <marianverehchak@gmail.com>
2025-08-21 01:11:34 +0300

    Оновлено main.tf: [Одна мережа-чистка портів/контейнерів/]

--- terraform-docker-test/main.tf
+++ terraform-docker-test/main.tf
@@ -11,21 +11,13 @@ provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
 
-# Мережа з імпортом існуючого ресурсу
 resource "docker_network" "jenkins_net" {
-  name       = "jenkins-network"
-  driver     = "bridge"
-  attachable = true
-
-  lifecycle {
-    ignore_changes = [name]  # Ігнорувати конфлікти імен
-  }
+  name = "jenkins-network"
+  driver = "bridge"
 }
 
-# Jenkins Master
 resource "docker_image" "jenkins_master" {
-  name         = "jenkins/jenkins:lts-jdk17"
-  keep_locally = true
+  name = "jenkins/jenkins:lts-jdk17"
 }
 
 resource "docker_container" "jenkins_master" {
@@ -33,18 +25,13 @@ resource "docker_container" "jenkins_master" {
   image = docker_image.jenkins_master.image_id
   
   networks_advanced {
-    name    = docker_network.jenkins_net.name
-    aliases = ["jenkins-master"]
+    name = docker_network.jenkins_net.name
   }
   
+  # АВТОМАТИЧНИЙ ВИБІР ПОРТУ (0 = Docker вибере вільний)
   ports {
     internal = 8080
-    external = 9080
-  }
-  
-  ports {
-    internal = 50000
-    external = 50001
+    external = 0
   }
   
   volumes {
@@ -53,58 +40,42 @@ resource "docker_container" "jenkins_master" {
   }
 
   restart = "unless-stopped"
-  shm_size = 1073741824 # 1GB
-  
-  healthcheck {
-    test     = ["CMD", "curl", "-f", "http://localhost:8080"]
-    interval = "30s"
-    timeout  = "10s"
-    retries  = 5
-  }
 }
 
-# Jenkins Agent
 resource "docker_image" "jenkins_agent" {
-  name         = "jenkins/inbound-agent:jdk17"
-  keep_locally = true
+  name = "jenkins/inbound-agent:jdk17"
 }
 
 resource "docker_container" "jenkins_agent" {
-  name     = "jenkins-agent"
-  image    = docker_image.jenkins_agent.image_id
+  name  = "jenkins-agent"
+  image = docker_image.jenkins_agent.image_id
   
   networks_advanced {
-    name    = docker_network.jenkins_net.name
-    aliases = ["jenkins-agent"]
+    name = docker_network.jenkins_net.name
   }
   
   env = [
     "JENKINS_URL=http://jenkins-master:8080",
     "JENKINS_SECRET=${var.jenkins_secret}",
     "JENKINS_AGENT_NAME=docker-agent",
-    "JENKINS_WEB_SOCKET=true",
-    "JENKINS_AGENT_WORKDIR=/home/jenkins/agent"
+    "JENKINS_WEB_SOCKET=true"
   ]
   
   volumes {
     host_path      = "/var/run/docker.sock"
     container_path = "/var/run/docker.sock"
   }
-  
-  volumes {
-    host_path      = "/home/acer/jenkins_agent_workdir"
-    container_path = "/home/jenkins/agent"
-  }
 
-  restart    = "on-failure"
-  memory     = 2147483648 # 2GB
-  cpu_shares = 512
-  
+  restart = "unless-stopped"
   depends_on = [docker_container.jenkins_master]
 }
 
 variable "jenkins_secret" {
-  description = "Agent connection secret from Jenkins UI"
+  description = "Secret from Jenkins UI"
   type        = string
-  sensitive   = true
+}
+
+# Output для отримання реального порту
+output "jenkins_url" {
+  value = "http://localhost:${docker_container.jenkins_master.ports[0].external}"
 }
